28a5b9801ada3f87b65e06883346ae4f10d47e9e
Thomas Cellerier <tcellerier@users.noreply.github.com>
2017-09-25 20:04:28 +0200

    update script_time_chauffage_sdb_consigne

--- script_time_chauffage_sdb_consigne.lua
+++ script_time_chauffage_sdb_consigne.lua
@@ -17,24 +17,25 @@ hysteresis = 0.3
 sdb_consigne_valeur = uservariables['Var_Chauffage_sdb_Consigne']
 sdb_consigne_onoff = otherdevices['Chauffage Sdb Consigne']
 sdb_onoff = otherdevices['Radiateur sdb On/Off']
-sdb_temp = otherdevices_temperature['Temp sdb']
-
+sdb_temp = otherdevices_temperature['Temp sdb'] or 18
+mode_maison = uservariables['Script_Mode_Maison']
 
 
 commandArray = {}
 
 
-if(sdb_consigne_onoff == 'On') then
+-- On active le chauffage de la sdb si le monde consigne est ON et si le monde maison n'est pas sur ABSENT
+if(sdb_consigne_onoff == 'On' and mode_maison ~= 'absent') then
 
     if (sdb_temp < sdb_consigne_valeur - hysteresis and sdb_onoff == 'Off') then
 
         commandArray['Radiateur sdb On/Off'] = 'On'
-        print('----- Chauffage sdb ON ----- Temp: '..sdb_temp)
+        print('----- Chauffage sdb ON ----- Temp: '.. math.round(sdb_temp, 1, ',') )
 
     elseif (sdb_temp >= sdb_consigne_valeur + hysteresis and sdb_onoff == 'On') then
 
         commandArray['Radiateur sdb On/Off'] = 'Off'
-        print('----- Chauffage sdb OFF ----- Temp: '..sdb_temp)
+        print('----- Chauffage sdb OFF ----- Temp: '.. math.round(sdb_temp, 1, ',') )
 
     end
 
