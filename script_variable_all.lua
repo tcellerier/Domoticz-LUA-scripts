18324be49d85f1325c9ad03d40ac0064ecf4ece8
Thomas Cellerier <tcellerier@users.noreply.github.com>
2017-09-28 20:56:27 +0200

    Update script_variable_all.lua

--- script_variable_all.lua
+++ script_variable_all.lua
@@ -12,8 +12,10 @@ commandArray = {}
 if (uservariablechanged['Script_Mode_Maison'] == 'auto') then
     tts_function('Mode Auto')
 
+
 elseif (uservariablechanged['Script_Mode_Maison'] == 'manuel') then
     tts_function('Mode Manuel')
+
 
 elseif (uservariablechanged['Script_Mode_Maison'] == 'absent') then
     tts_function('Mode absent')
@@ -25,6 +27,12 @@ elseif (uservariablechanged['Script_Mode_Maison'] == 'absent') then
     
     -- On ferme le volet de la salle de bain
     commandArray['Volets sdb'] = 'Off'
+
+    -- On active la caméra au bout de 30 min
+    if (otherdevices['Camera'] == 'Off') then
+        commandArray['Camera'] = 'On AFTER 1800'
+        tts_function('Caméra armée dans 30 minutes')
+    end
     
 
 elseif (uservariablechanged['Script_Mode_Volets'] == 'auto' and uservariables['Script_Mode_VoletsTardifs'] == 'off') then
