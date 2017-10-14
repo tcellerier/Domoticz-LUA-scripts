3b66c3948d5842d5055131e76ef33fab888a8a0a
Thomas Cellerier <tcellerier@users.noreply.github.com>
2017-10-14 19:18:36 +0200

    Update script_time_color_change.lua

--- script_time_color_change.lua
+++ script_time_color_change.lua
@@ -3,7 +3,7 @@
 ------------------------------------------------------
 
 -------------------------------------------------------------
---  Change la couleur de la lampe toutes les 20 secondes pendant 1 heure
+--  Change la couleur de la lampe toutes les X secondes pendant 1 heure
 --  Conditions : Device 'Lampe chambre Color Change' = On
 --               (une fois activé le device le reste 1h)
 -------------------------------------------------------------
@@ -22,10 +22,10 @@ if (otherdevices['Lampe chambre Color Change'] == 'On') then
 
 
     idx = 66  -- identifiant domoticz de la lampe
-    brightness = uservariables['Script_Lamp_brightness']  -- intensité de 1 à 100
-    hue = uservariables['Script_Lamp_color_hue']  -- couleur actuelle (de 0 à 359)
+    brightness = uservariables['Script_Lamp_brightness'] or 70 -- intensité de 1 à 100
+    hue = uservariables['Script_Lamp_color_hue'] or 100 -- couleur actuelle (de 0 à 359)
     first_hue = tostring((hue  + 1) % 360)
-    frequence = uservariables['Script_Lamp_Freq_Change']  -- Fréquence de change des couleurs / minute (de 1 à 6)
+    frequence = uservariables['Script_Lamp_Freq_Change'] or 1  -- Fréquence de change des couleurs / minute (de 1 à 6)
     server = '127.0.0.1' 
     -- domoticzCredentials dans la library library_credentials
 
